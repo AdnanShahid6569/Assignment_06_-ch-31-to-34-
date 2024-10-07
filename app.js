9f010b52f7d649112c318909e88fbabc52b6cd1e
AdnanShahidBakkar <adnanshahidbakkar@gmail.com>
2024-10-07 16:43:07 +0500

    complete

--- app.js
+++ app.js
@@ -2,149 +2,149 @@
 
 // Questions
 
-// // q 01
-// var date = new Date();
+// q 01
+var date = new Date();
 
-// console.log(date);
+console.log(date);
 
 
 
 
-// // q 02
-// var monthList =["january","feburary","March","April","May","june","july","August","september","october","nevember","december"]
+// q 02
+var monthList =["january","feburary","March","April","May","june","july","August","september","october","nevember","december"]
 
-// var month = new Date();
+var month = new Date();
 
-// console.log(monthList[month.getMonth()]);
+console.log(monthList[month.getMonth()]);
 
 
 
-// // q 03
-// var days = ["Sunday","Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"];
+// q 03
+var days = ["Sunday","Monday","Tuesday","Wednsday","Thursday","Friday","Saturday"];
 
-// var day = new Date();
+var day = new Date();
 
-// var current = (days[day.getDay()]);
+var current = (days[day.getDay()]);
 
-// console.log("Today is " + current.slice(0,3));
+console.log("Today is " + current.slice(0,3));
 
 
 
-// // q 04
-// var day = new Date();
+// q 04
+var day = new Date();
 
-// if(day === "Saturday" || day === "Sunday"){
-//     alert("Its Funday")
-// }
-// else{
-//     alert("Its Work Day")
-// }
+if(day === "Saturday" || day === "Sunday"){
+    alert("Its Funday")
+}
+else{
+    alert("Its Work Day")
+}
 
 
 
 
-// // q 05
-// var date = new Date().getDate();
+// q 05
+var date = new Date().getDate();
 
-// if(date < 16){
-//     alert("First fifteen days of the month")
-// }
-// else{
-//     alert("Last Days of the Month")
-// }
+if(date < 16){
+    alert("First fifteen days of the month")
+}
+else{
+    alert("Last Days of the Month")
+}
 
 
 
 
-// // q 06
-// var date = new Date();
+// q 06
+var date = new Date();
 
-// console.log("Current Date : " + date);
+console.log("Current Date : " + date);
 
-// var currMillis = date.getTime();
-// console.log("Elapsed MilliSecond since January 1 : " + currMillis );
+var currMillis = date.getTime();
+console.log("Elapsed MilliSecond since January 1 : " + currMillis );
 
-// var time = date.getTime();
-// console.log("Elapsed MilliSecond since January 1 : " + time);
+var time = date.getTime();
+console.log("Elapsed MilliSecond since January 1 : " + time);
 
 
 
 
-// // q 07
-// var hour = new Date();
+// q 07
+var hour = new Date();
 
-// var update = hour.getHours();
+var update = hour.getHours();
 
-// if(hour >= 12){
-//     alert("Current Time : " + update + " AM");
-// }
-// else{
-//     alert("Current Time : " + update + " PM")
-// }
+if(hour >= 12){
+    alert("Current Time : " + update + " AM");
+}
+else{
+    alert("Current Time : " + update + " PM")
+}
 
 
 
 
-// // q 08
-// var laterDate = new Date("2020 Dec 31");
+// q 08
+var laterDate = new Date("2020 Dec 31");
 
-// console.log(laterDate);
+console.log(laterDate);
 
 
 
 
-// // q 09
-// pastRamazan = new Date("2024 3 8");
+// q 09
+pastRamazan = new Date("2024 3 8");
 
-// var ramazan = new Date();
+var ramazan = new Date();
 
-// var diff = (pastRamazan - ramazan );
+var diff = (pastRamazan - ramazan );
 
-// var day = diff / (1000*60*60*24);
+var day = diff / (1000*60*60*24);
 
-// console.log("Past Ramazan before Days : " + Math.round(Math.abs(day)));
+console.log("Past Ramazan before Days : " + Math.round(Math.abs(day)));
 
 
 
 
 
-// // q 10
-// var date = new Date();
-// console.log(date);
+// q 10
+var date = new Date();
+console.log(date);
 
 
-// var startPoint = new Date("2015 January 01");
+var startPoint = new Date("2015 January 01");
 
-// var diff = date - startPoint;
+var diff = date - startPoint;
 
-// var update = diff / 1000;
+var update = diff / 1000;
 
-// console.log(update);
+console.log(update);
 
 
 
 
-// // q 11
-// var date = new Date();
+// q 11
+var date = new Date();
 
-// var up = date.getHours();
+var up = date.getHours();
 
-// date.setHours(up +1);
-// console.log(date);
+date.setHours(up +1);
+console.log(date);
 
 
 
 
-// // q 12
-// // q 13
+// q 12
+// q 13
 
-// var user = prompt("Enter Your Sun");
+var user = prompt("Enter Your Sun");
 
-// var year = new Date().getFullYear();
+var year = new Date().getFullYear();
 
-// console.log(`Your Age is : ${ year - user}`);
+console.log(`Your Age is : ${ year - user}`);
 
-// console.log(`Your Birth Year is : ${user}`);
+console.log(`Your Birth Year is : ${user}`);
 
 
 
